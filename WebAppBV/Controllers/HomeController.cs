393f7a21965b4782e178011dc043be51dc2a06d7
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2022-04-06 20:21:48 -0300

    Add project files.

---
+++ WebAppBV/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppBV.Models;
+
+namespace WebAppBV.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHostingEnvironment _appEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment env)
+        {
+            _logger = logger;
+            _appEnvironment = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //método para enviar os arquivos usando a interface IFormFile
+        public async Task<IActionResult> ExportFile(List<IFormFile> arquivos)
+        {
+            long tamanhoArquivos = arquivos.Sum(f => f.Length);
+            // caminho completo do arquivo na localização temporária
+            var caminhoArquivo = Path.GetTempFileName();
+
+            // processa os arquivo enviados
+            //percorre a lista de arquivos selecionados
+            foreach (var arquivo in arquivos)
+            {
+                //verifica se existem arquivos 
+                if (arquivo == null || arquivo.Length == 0)
+                {
+                    //retorna a viewdata com erro
+                    ViewData["Erro"] = "Error: Arquivo(s) não selecionado(s)";
+                    return View(ViewData);
+                }
+                // < define a pasta onde vamos salvar os arquivos >
+                string pasta = "Arquivos_Usuario";
+                // Define um nome para o arquivo enviado incluindo o sufixo obtido de milesegundos
+                string nomeArquivo = "Usuario_arquivo_" + DateTime.Now.Millisecond.ToString();
+                //verifica qual o tipo de arquivo : jpg, gif, png, pdf ou tmp
+                if (arquivo.FileName.Contains(".jpg"))
+                    nomeArquivo += ".jpg";
+                else if (arquivo.FileName.Contains(".gif"))
+                    nomeArquivo += ".gif";
+                else if (arquivo.FileName.Contains(".png"))
+                    nomeArquivo += ".png";
+                else if (arquivo.FileName.Contains(".pdf"))
+                    nomeArquivo += ".pdf";
+                else
+                    nomeArquivo += ".tmp";
+                //< obtém o caminho físico da pasta wwwroot >
+                string caminho_WebRoot = _appEnvironment.WebRootPath;
+                // monta o caminho onde vamos salvar o arquivo : 
+                // ~\wwwroot\Arquivos\Arquivos_Usuario\Recebidos
+                string caminhoDestinoArquivo = caminho_WebRoot + "\\Arquivos\\" + pasta + "\\";
+                // incluir a pasta Recebidos e o nome do arquivo enviado : 
+                // ~\wwwroot\Arquivos\Arquivos_Usuario\Recebidos\
+                string caminhoDestinoArquivoOriginal = caminhoDestinoArquivo + "\\Recebidos\\" + nomeArquivo;
+                //copia o arquivo para o local de destino original
+                using (var stream = new FileStream(caminhoDestinoArquivoOriginal, FileMode.Create))
+                {
+                    await arquivo.CopyToAsync(stream);
+                }
+            }
+            //monta a ViewData que será exibida na view como resultado do envio 
+            ViewData["Resultado"] = $"{arquivos.Count} arquivos foram enviados ao servidor, " +
+             $"com tamanho total de : {tamanhoArquivos} bytes";
+            //retorna a viewdata
+            return View(ViewData);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+    }
+}
