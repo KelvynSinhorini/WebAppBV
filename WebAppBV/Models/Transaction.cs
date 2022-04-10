a81d459bdfdc97e5956e700d92284fe84aa6f21d
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2022-04-10 16:30:41 -0300

    Adicionado Model Transaction

---
+++ WebAppBV/Models/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WebAppBV.Models
+{
+    public class Transaction
+    {
+        //[Required]
+        //[Column("TransactionId")]
+        //public Guid TransactionId { get; set; }
+
+        //[Column("Description")]
+        public string Description { get; set; }
+        //[Column("Local")]
+        public string Local { get; set; }
+        //[Column("Value")]
+        public decimal Value { get; set; }
+        //[Column("Date")]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Numero da parcela
+        /// </summary>
+        //[Column("NumberOfParcel")]
+        public int? NumberOfParcel { get; set; }
+
+        /// <summary>
+        /// Quantidade de parcelas (Total)
+        /// </summary>
+        //[Column("TotalParcel")]
+        public int? TotalParcel { get; set; }
+        //[Column("OnlyThisMonth")]
+        public bool OnlyThisMonth { get; set; } = false;
+    }
+}
