bc23ad982227593e87e12e367935efd91fba2068
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2022-08-15 19:24:32 -0300

    Adicionado conversores

---
+++ WebAppBV/Helpers/Converters.cs
@@ -0,0 +1,24 @@
+﻿using WebAppBV.Models;
+using WebAppBV.ViewModels;
+
+namespace WebAppBV.Helpers
+{
+    public static class Converters
+    {
+        public static TransactionViewModel ConvertTransactionToViewModel(Transaction transaction)
+        {
+            var transactionViewModel = new TransactionViewModel(transaction.TransactionId, transaction.Description, transaction.Local, 
+                transaction.Value, transaction.Date, transaction.NumberOfParcel, transaction.TotalParcel, transaction.OnlyThisMonth, transaction.Owner);
+
+            return transactionViewModel;
+        }
+
+        public static Transaction ConvertTransactionViewModelToModel(TransactionViewModel transactionViewModel)
+        {
+            var transaction = new Transaction(transactionViewModel.TransactionId, transactionViewModel.Description, transactionViewModel.Local,
+                transactionViewModel.Value, transactionViewModel.Date, transactionViewModel.NumberOfParcel, transactionViewModel.TotalParcel, transactionViewModel.OnlyThisMonth, transactionViewModel.Owner);
+
+            return transaction;
+        }
+    }
+}
